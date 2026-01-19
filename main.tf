6722631c35fdfc2bd6377c24ec9e1226fb18006c
yeshwanthkr12 <136964381+yeshwanthkr12@users.noreply.github.com>
2026-01-20 01:48:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet" "subnet"{
 name = "subnet"
 resource_group_name = data.azurerm_resource_group.rg.name
 virtual_network_name = azurerm_virtual_network.vnet.name
-address_prefixes = ["10.0.1.0/16"]
+address_prefixes = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
