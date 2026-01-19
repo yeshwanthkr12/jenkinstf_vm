f0227f5d0f724b8026e47b2707260ea47a9229f7
yeshwanthkr12 <136964381+yeshwanthkr12@users.noreply.github.com>
2026-01-20 00:41:06 +0530

    Define Azure infrastructure in main.tf
    Added Azure resources including a resource group, virtual network, subnet, network interface, and a Linux virtual machine.

--- main.tf
+++ main.tf
@@ -1,1 +1,56 @@
+data "azurerm_resource_group" "rg" {
+name = "balaazgrp"
+}
 
+resource "azurerm_virtual_network" "vnet"{
+name = "tf-vnet"
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet"{
+name = "subnet"
+resource_group_name = azurerm_resource_group.rg.name
+virtual_network_name = azurerm_virtual_network.vnet.name
+address_prefixes = ["10.0.1.0/16"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
