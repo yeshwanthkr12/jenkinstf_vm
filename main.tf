109a7b97289e5e8aca510335151b6f547fc9ab26
yeshwanthkr12 <136964381+yeshwanthkr12@users.noreply.github.com>
2026-01-20 01:28:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   ]
 admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("/home/devops/.ssh/id_rsa.pub")
   }
 
   os_disk {
