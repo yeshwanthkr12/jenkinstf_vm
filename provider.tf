f2f343823e69adde37df409ff285bc3cc3e3042d
yeshwanthkr12 <136964381+yeshwanthkr12@users.noreply.github.com>
2026-01-20 00:13:07 +0530

    Configure Azure RM provider in Terraform
    Add Terraform provider configuration for Azure RM.

--- provider.tf
+++ provider.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.57.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  # Configuration options
+}
